0a89230beef9404ead8c9987459f70aa46eca91c
thonglor2505 <54268583+thonglor2505@users.noreply.github.com>
2021-01-16 08:39:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'd4RK33gTTTGWvOZP0AloggELk4d77bvFvKrvqqKkqvwT7htuaKsvlhkGyW0naT++Kb2AtVWth35/jl3qsiZYOZF7sXUk83GHGU2VvLrsWAwNTKwYskdA8FcVHCu0cJ5Nm5fWoSin6NfDeKZO9OWVmwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
